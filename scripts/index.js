ca72ffe69694eeb7912d9eed13dbc010262a4e64
PavelPavelPavelPavel <paveliva1988@gmail.com>
2023-05-04 20:07:08 +0300

    fix: html and Js

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,27 @@
 // Находим форму в DOM
-let formElement = document.querySelector('.popup__container');
+let formElement = document.querySelector('.popup__form');
 // Находим поля формы в DOM
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__profession');
+let nameInput = document.querySelector('.popup__value_field_name');
+let jobInput = document.querySelector('.popup__value_field_job');
 // Находим кнопку открытия popup
 let openPopupButton = document.querySelector('.profile__button');
  // Выбираем поля с именем и профессией
  let profileName = document.querySelector('.profile__name');
  let profileJob = document.querySelector('.profile__work');
-// Находдим кнопку закрытия popup
+// Находим кнопку закрытия popup
 const closePopupButton = document.querySelector('.popup__button-close');
 // находим popup
 const popup = document.querySelector('.popup');
 // функция закрытия popup
 let popupToggle = (popup) => ('click', popup.classList.toggle('popup_opened'));
-// открываем popup и подставояем значения из profileName и profileJob
-openPopupButton.addEventListener('click', function() {
+// функция открытия popup 
+function openPopup() {
     popupToggle(popup);
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
-});
+}
+// открываем popup и подставояем значения из profileName и profileJob при помоще функции openPopup
+openPopupButton.addEventListener('click', openPopup);
 //закрываем popup без сохранения
 closePopupButton.addEventListener('click', () => popupToggle(popup));
 // Обработчик «отправки» формы, хотя пока
